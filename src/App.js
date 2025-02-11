9caa2e341f31d13d38d522b74a79d7c5ba1612b2
Thomas Krantz <thomas.krantz.90@gmail.com>
2025-02-11 10:55:33 +0100

    css structure added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 // 4e226c3d20142e7c294fb52e075411ef
+import s from "./style.module.css";
+import "./global.css";
 function App() {
     return (
-        <div>
-            <h1>MovieRate</h1>
+        <div className={s.main_container}>
+            <div className={s.header}>
+                <div className="row">
+                    <div className="col-4">
+                        <div>Logo</div>
+                        <div>Subtitle</div>
+                    </div>
+                    <div className="col-md-12 col-lg-4">
+                        <input type="text" />
+                    </div>
+                </div>
+            </div>
+            <div className={s.tv_show_detail}>Detail</div>
+            <div className={s.recommentations}>Recommendations</div>
         </div>
     );
 }
